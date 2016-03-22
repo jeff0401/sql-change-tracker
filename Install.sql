02fe1677a9fe6c4d702f11722b1a2739f5540acc
jeff0401 <polk.jeffrey@gmail.com>
2016-03-21 21:52:11 -0400

    Updated for 2.0

--- Install.sql
+++ Install.sql
@@ -18,8 +18,102 @@ INSERT INTO dbo.TrackChangeConfig (SchemaName, TableName, IdentityIncrement)
 SELECT s.name, t.name, CASE WHEN c.column_id IS NULL THEN NULL ELSE 1000 END
 FROM sys.tables t INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
 LEFT OUTER JOIN sys.identity_columns c ON t.object_id = c.object_id
+WHERE s.name + '.' + t.name <> 'dbo.TrackChangeConfig'
 GO
 
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[dbo].[uspTrackChangeStart]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [dbo].[uspTrackChangeStart]
+END
+GO
+CREATE PROCEDURE [dbo].[uspTrackChangeStart]
+AS
+BEGIN
+		
+	DECLARE @DB varchar(100), @Schema varchar(100), @Table varchar(100)
+	DECLARE @IdentityIncrement int, @IdentitySeed int, @IdentityValue int
+	DECLARE @FullTable varchar(205)
+
+	SET @DB = DB_NAME();
+	
+	IF EXISTS (SELECT * 
+		FROM sys.change_tracking_databases 
+		WHERE database_id = DB_ID(@DB)) BEGIN
+		
+		PRINT 'tracking already enabled on the database'
+	END ELSE BEGIN
+		PRINT 'activating database level'
+		EXEC('ALTER DATABASE [' + @DB + '] SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 365 DAYS, AUTO_CLEANUP = ON);')
+	END
+
+	DECLARE cur CURSOR FOR
+		SELECT SchemaName, TableName, IdentityIncrement FROM dbo.TrackChangeConfig WHERE TrackChanges = 1 ORDER BY SchemaName, TableName
+	OPEN cur
+	FETCH NEXT FROM cur INTO @Schema, @Table, @IdentityIncrement
+	WHILE @@FETCH_STATUS = 0 BEGIN
+		
+		SET @FullTable = '[' + @Schema + '].[' + @Table + ']';
+
+		IF EXISTS (SELECT * FROM sys.change_tracking_tables WHERE object_id = OBJECT_ID(@FullTable)) BEGIN
+			PRINT 'tracking already enabled on ' + @FullTable
+		END ELSE BEGIN
+			IF @IdentityIncrement > 0 BEGIN
+				SET @IdentitySeed = IDENT_SEED(@FullTable);
+				SET @IdentityValue = IDENT_CURRENT(@FullTable);
+				SET @IdentityValue = @IdentityIncrement + CASE WHEN @IdentitySeed > @IdentityValue THEN @IdentitySeed ELSE @IdentityValue END;
+				PRINT 'reseeding ' + @FullTable + ' to ' + CONVERT(VARCHAR(50), @IdentityValue)
+				DBCC CHECKIDENT(@FullTable, RESEED, @IdentityValue) WITH NO_INFOMSGS;
+			END
+			PRINT 'activating ' + @FullTable
+			EXEC('ALTER TABLE ' + @FullTable + ' ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = OFF);')
+		END
+		
+		FETCH NEXT FROM cur INTO @Schema, @Table, @IdentityIncrement
+	END
+	CLOSE cur
+	DEALLOCATE cur
+END
+GO
+
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[dbo].[uspTrackChangeStop]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [dbo].[uspTrackChangeStop]
+END
+GO
+CREATE PROCEDURE [dbo].[uspTrackChangeStop]
+AS
+BEGIN
+		
+	DECLARE @DB varchar(100), @Schema varchar(100), @Table varchar(100) 
+	SET @DB = db_name()
+
+	DECLARE cur CURSOR FOR
+		SELECT s.name, t.name
+		FROM sys.tables t INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+		INNER JOIN sys.change_tracking_tables ct ON t.object_id = ct.object_id
+	OPEN cur
+	FETCH NEXT FROM cur INTO @Schema, @Table
+	WHILE @@FETCH_STATUS = 0 BEGIN
+		PRINT 'deactivating [' + @Schema + '].[' + @Table + ']'
+		EXEC('ALTER TABLE [' + @Schema + '].[' + @Table + '] DISABLE CHANGE_TRACKING;')
+		
+		FETCH NEXT FROM cur INTO @Schema, @Table
+	END
+	CLOSE cur
+	DEALLOCATE cur
+
+	PRINT 'deactivating database level'
+	EXEC('ALTER DATABASE [' + @DB + '] SET CHANGE_TRACKING = OFF;')
+	
+END
+GO
 
 IF EXISTS ( SELECT * 
             FROM   sysobjects 
@@ -509,7 +603,6 @@ END
 
 GO
 
-
 IF EXISTS ( SELECT * 
             FROM   sysobjects 
             WHERE  id = object_id(N'[dbo].[uspTrackChangeExtract]') 
@@ -525,17 +618,27 @@ BEGIN
 
 	DECLARE @DB varchar(100), @Schema varchar(100), @Table varchar(100)
 	DECLARE @PK1 varchar(100), @PK2 varchar(100), @PK3 varchar(100) 
-	DECLARE @PK1Val varchar(100), @PK2Val varchar(100) , @PK3Val varchar(100) 
+	DECLARE @PK1Val varchar(max), @PK2Val varchar(max) , @PK3Val varchar(max) 
+	DECLARE @PK1Type varchar(100), @PK2Type varchar(100), @PK3Type varchar(100) 
 	DECLARE @SQL varchar(4000)
-	DECLARE @FullTable varchar(200)
-	DECLARE @SearchCondition varchar(500), @PopulateIdentity bit
-	CREATE TABLE #PKVals (PKVal1 varchar(500), PKVal2 varchar(500), PKVal3 varchar(500))
+	DECLARE @FullTable varchar(500)
+	DECLARE @SearchCondition varchar(4000), @PopulateIdentity bit
 
-	DECLARE cur cursor for
+	CREATE TABLE #PKVals (PKVal1 varchar(max), PKVal2 varchar(max), PKVal3 varchar(max))
+
+	-- set up transaction
+	PRINT 'BEGIN TRANSACTION;'
+	PRINT 'BEGIN TRY'
+
+	-- disable FKs
+	PRINT 'EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all";'
+	PRINT ''
+
+	DECLARE cur CURSOR FOR
 		SELECT SchemaName, TableName FROM dbo.TrackChangeConfig WHERE TrackChanges = 1
-	open cur
-	fetch next from cur into @Schema, @Table
-	while @@FETCH_STATUS = 0 begin
+	OPEN cur
+	FETCH NEXT FROM cur INTO @Schema, @Table
+	WHILE @@FETCH_STATUS = 0 BEGIN
 	
 		SET @FullTable = '[' + @Schema + '].[' + @Table + ']'
 	
@@ -567,46 +670,84 @@ BEGIN
 		  FOR KeyPart IN (PK1, PK2, PK3)
 		) piv
 
+		-- get the types for the PKs
+		SELECT @PK1Type = ty.name
+		FROM sys.tables t
+		INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+		INNER JOIN sys.columns c ON t.object_id = c.object_id
+		INNER JOIN sys.types ty ON c.system_type_id = ty.system_type_id
+		WHERE s.name = @Schema
+		AND t.name = @Table
+		AND c.name = @PK1
 		
+		SELECT @PK2Type = ty.name
+		FROM sys.tables t
+		INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+		INNER JOIN sys.columns c ON t.object_id = c.object_id
+		INNER JOIN sys.types ty ON c.system_type_id = ty.system_type_id
+		WHERE s.name = @Schema
+		AND t.name = @Table
+		AND c.name = @PK2
+
+		SELECT @PK3Type = ty.name
+		FROM sys.tables t
+		INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+		INNER JOIN sys.columns c ON t.object_id = c.object_id
+		INNER JOIN sys.types ty ON c.system_type_id = ty.system_type_id
+		WHERE s.name = @Schema
+		AND t.name = @Table
+		AND c.name = @PK3
+
 		DELETE FROM #PKVals
-		set @SQL = 'insert into #PKVals (PKVal1, PKVal2, PKVal3) select ' + @PK1 + ', ' + COALESCE(@PK2, '1') + ', ' + COALESCE(@PK3, '1')
-		set @SQL = @SQL + ' FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT'
+		SET @SQL = 'insert into #PKVals (PKVal1, PKVal2, PKVal3) select ' + @PK1 + ', ' + COALESCE(@PK2, '1') + ', ' + COALESCE(@PK3, '1')
+		SET @SQL = @SQL + ' FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT'
 		--print @SQL
-		exec(@SQL)
+		EXEC(@SQL)
 	
-	
-		if exists (select top 1 * from #PKVals) begin
-			DECLARE change cursor for 
-				select PKVal1, PKVal2, PKVal3 from #PKVals
-			open change
-			fetch next from change into @PK1Val, @PK2Val, @PK3Val
-			while @@FETCH_STATUS = 0 begin
+		IF EXISTS (SELECT TOP 1 * FROM #PKVals) BEGIN
+			DECLARE change CURSOR FOR
+				SELECT PKVal1, PKVal2, PKVal3 FROM #PKVals
+			OPEN change
+			FETCH NEXT FROM change INTO @PK1Val, @PK2Val, @PK3Val
+			WHILE @@FETCH_STATUS = 0 BEGIN
 				--write delete statement
-				set @SQL = 'DELETE FROM ' + @FullTable + ' WHERE ' + @PK1 + ' = ''' + @PK1Val + ''''
-				if @PK2 is not null begin
-					set @SQL = @SQL + ' AND ' + @PK2 + ' = ''' + @PK2Val + ''''
-				end
-				if @PK3 is not null begin
-					set @SQL = @SQL + ' AND ' + @PK3 + ' = ''' + @PK3Val + ''''
-				end
-				print @SQL
-				fetch next from change into @PK1Val, @PK2Val, @PK3Val
-			end
-			close change
-			deallocate change
+				SET @SQL = 'DELETE FROM ' + @FullTable + ' WHERE ' + @PK1 + ' = '
+				SET @SQL = @SQL + CASE 
+					WHEN @PK1Type IN ('int','bigint','smallint','tinyint','decimal','numeric','bit') THEN @PK1Val
+					ELSE '''' + REPLACE(@PK1Val, '''', '''''') + ''''
+				END
+				IF @PK2 IS NOT NULL BEGIN
+					SET @SQL = @SQL + ' AND ' + @PK2 + ' = '
+					SET @SQL = @SQL + CASE 
+						WHEN @PK2Type IN ('int','bigint','smallint','tinyint','decimal','numeric','bit') THEN @PK2Val
+						ELSE '''' + REPLACE(@PK2Val, '''', '''''') + ''''
+					END
+				END
+				IF @PK3 IS NOT NULL BEGIN
+					SET @SQL = @SQL + ' AND ' + @PK3 + ' = '
+					SET @SQL = @SQL + CASE 
+						WHEN @PK3Type IN ('int','bigint','smallint','tinyint','decimal','numeric','bit') THEN @PK3Val
+						ELSE '''' + REPLACE(@PK3Val, '''', '''''') + ''''
+					END
+				END
+				PRINT @SQL
+				FETCH NEXT FROM change INTO @PK1Val, @PK2Val, @PK3Val
+			END
+			CLOSE change
+			DEALLOCATE change
 
 			-- generate inserts
-			if @PK3 is not null begin
-				set @SearchCondition = 'EXISTS (SELECT * FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT WHERE ' + @FullTable + '.' + @PK1 + ' = CT.' + @PK1 + ' AND ' + @FullTable + '.' + @PK2 + ' = CT.' + @PK2 + ' AND ' + @FullTable + '.' + @PK3 + ' = CT.' + @PK3 + ')'
-			end else if @PK2 is not null begin
-				set @SearchCondition = 'EXISTS (SELECT * FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT WHERE ' + @FullTable + '.' + @PK1 + ' = CT.' + @PK1 + ' AND ' + @FullTable + '.' + @PK2 + ' = CT.' + @PK2 + ')'
-			end else begin
-				set @SearchCondition = @PK1 + ' IN (SELECT ' + @PK1 + ' FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT)'
-			end
-			print @SearchCondition
+			IF @PK3 IS NOT NULL BEGIN
+				SET @SearchCondition = 'EXISTS (SELECT * FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT WHERE ' + @FullTable + '.' + @PK1 + ' = CT.' + @PK1 + ' AND ' + @FullTable + '.' + @PK2 + ' = CT.' + @PK2 + ' AND ' + @FullTable + '.' + @PK3 + ' = CT.' + @PK3 + ')'
+			END ELSE IF @PK2 is not null BEGIN
+				SET @SearchCondition = 'EXISTS (SELECT * FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT WHERE ' + @FullTable + '.' + @PK1 + ' = CT.' + @PK1 + ' AND ' + @FullTable + '.' + @PK2 + ' = CT.' + @PK2 + ')'
+			END ELSE BEGIN
+				SET @SearchCondition = @PK1 + ' IN (SELECT ' + @PK1 + ' FROM CHANGETABLE(CHANGES ' + @FullTable + ', 0) AS CT)'
+			END
+			--print @SearchCondition
 
 		
-			exec uspTrackChangeGenerateInsert
+			EXEC uspTrackChangeGenerateInsert
 				@ObjectName = @FullTable,
 				@GenerateSingleInsertPerRow = 1, 
 				@GenerateProjectInfo = 0,
@@ -614,84 +755,25 @@ BEGIN
 				@OmmitUnsupportedDataTypes = 0,
 				@GenerateSetNoCount = 0,
 				@SearchCondition = @SearchCondition
-
-			
-		end
-		
-		fetch next from cur into @Schema, @Table
-	end
-	close cur
-	deallocate cur
-
-	drop table #PKVals	
-	
-END
-GO
-
-IF EXISTS ( SELECT * 
-            FROM   sysobjects 
-            WHERE  id = object_id(N'[dbo].[uspTrackChangeStart]') 
-                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
-BEGIN
-    DROP PROCEDURE [dbo].[uspTrackChangeStart]
-END
-GO
-CREATE PROCEDURE [dbo].[uspTrackChangeStart]
-AS
-BEGIN
-		
-	DECLARE @DB varchar(100), @Schema varchar(100), @Table varchar(100) 
-	SET @DB = db_name()
-
-	PRINT 'activating database level'
-	EXEC('ALTER DATABASE [' + @DB + '] SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 365 DAYS, AUTO_CLEANUP = ON);')
-	
-	DECLARE cur CURSOR FOR
-		SELECT SchemaName, TableName FROM dbo.TrackChangeConfig WHERE TrackChanges = 1
-	OPEN cur
-	FETCH NEXT FROM cur INTO @Schema, @Table
-	WHILE @@FETCH_STATUS = 0 BEGIN
-		PRINT 'activating [' + @Schema + '].[' + @Table + ']'
-		EXEC('ALTER TABLE [' + @Schema + '].[' + @Table + '] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = OFF);')
-		FETCH NEXT FROM cur INTO @Schema, @Table
-	END
-	CLOSE cur
-	DEALLOCATE cur
-END
-GO
-
-IF EXISTS ( SELECT * 
-            FROM   sysobjects 
-            WHERE  id = object_id(N'[dbo].[uspTrackChangeStop]') 
-                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
-BEGIN
-    DROP PROCEDURE [dbo].[uspTrackChangeStop]
-END
-GO
-CREATE PROCEDURE [dbo].[uspTrackChangeStop]
-AS
-BEGIN
-		
-	DECLARE @DB varchar(100), @Schema varchar(100), @Table varchar(100) 
-	SET @DB = db_name()
-
-	DECLARE cur CURSOR FOR
-		SELECT s.name, t.name
-		FROM sys.tables t INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
-		INNER JOIN sys.change_tracking_tables ct ON t.object_id = ct.object_id
-	OPEN cur
-	FETCH NEXT FROM cur INTO @Schema, @Table
-	WHILE @@FETCH_STATUS = 0 BEGIN
-		PRINT 'deactivating [' + @Schema + '].[' + @Table + ']'
-		EXEC('ALTER TABLE [' + @Schema + '].[' + @Table + '] DISABLE CHANGE_TRACKING;')
+		END
 		
 		FETCH NEXT FROM cur INTO @Schema, @Table
 	END
 	CLOSE cur
 	DEALLOCATE cur
 
-	PRINT 'deactivating database level'
-	EXEC('ALTER DATABASE [' + @DB + '] SET CHANGE_TRACKING = OFF;')
+	DROP TABLE #PKVals	
+	
+	-- enable FKs
+	PRINT 'EXEC sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"';
+	
+	-- handle transaction
+	PRINT 'END TRY'
+	PRINT 'BEGIN CATCH'
+	PRINT '	SELECT ERROR_NUMBER() AS ErrorNumber,ERROR_SEVERITY() AS ErrorSeverity,ERROR_STATE() AS ErrorState,ERROR_PROCEDURE() AS ErrorProcedure,ERROR_LINE() AS ErrorLine,ERROR_MESSAGE() AS ErrorMessage;'
+	PRINT '	IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;'
+	PRINT 'END CATCH'
+	PRINT 'IF @@TRANCOUNT > 0 COMMIT TRANSACTION;'
 	
 END
 GO
--- Uninstall.sql
+++ Uninstall.sql
@@ -1,12 +1,12 @@
-EXEC uspTrackChangeStop
+EXEC [dbo].[uspTrackChangeStop]
 GO
-DROP PROCEDURE dbo.uspTrackChangeStart
+DROP PROCEDURE [dbo].[uspTrackChangeStart]
 GO
-DROP PROCEDURE dbo.uspTrackChangeStop
+DROP PROCEDURE [dbo].[uspTrackChangeStop]
 GO
-DROP PROCEDURE dbo.uspTrackChangeExtract
+DROP PROCEDURE [dbo].[uspTrackChangeExtract]
 GO
-DROP PROCEDURE dbo.uspTrackChangeGenerateInsert
+DROP PROCEDURE [dbo].[uspTrackChangeGenerateInsert]
 GO
 DROP TABLE [dbo].[TrackChangeConfig]
 GO
